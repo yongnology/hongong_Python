e1aec13be46a7a706be97ec0e202ee169f1335c3
yongnology <cassius20@naver.com>
2022-05-22 00:09:59 +0900

    03-2 if~else와 elif 구문

---
+++ chap03/sec02/false_value.py
@@ -0,0 +1,23 @@
+# False로 변환되는 값
+
+print("# if 조건문에 0 넣기")
+if 0:
+  print("0은 Ture로 변환됩니다.")
+else:
+  print("0은 False로 변환됩니다.")
+print()
+
+print("# if 조건문에 빈 문자열 넣기")
+if "" :
+  print("빈 문자열은 True로 변환됩니다.")
+else :
+  print("빈 문자열은 False로 변환됩니다.")
+
+'''
+# 출력값
+# if 조건문에 0 넣기
+0은 False로 변환됩니다.
+
+# if 조건문에 빈 문자열 넣기
+빈 문자열은 False로 변환됩니다.
+'''
